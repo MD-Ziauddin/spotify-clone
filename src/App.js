8c23f5f509bc04ee09ae100328e23ccadf70fc5c
Ziauddin <md.ziauddin02@gmail.com>
2021-01-30 18:41:55 +0530

    Context API added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
 
 import Login from "./component/Login";
+import Player from "./component/Player";
 import { getTokenFromUrl } from "./spotify";
+import { useStateValue } from "./StateProvider";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{}, dispatch] = useStateValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -20,20 +23,14 @@ function App() {
       // spotify.setAccessToken(_token);
       spotify.setAccessToken(_token);
 
-      spotify
-        .getMe()
-        .then((user) => {
-          console.log(user);
-        })
-        .catch((err) => console.log(err));
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
+      console.log("i have the token ", _token);
     }
-
-    console.log("i have the token ", _token);
   }, []);
 
-  return (
-    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
-  );
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/component/Player.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Player() {
+  return <div>Welcome to SPOTIFY</div>;
+}
+
+export default Player;
